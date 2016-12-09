9b9ca71c59ee6985e9d58ee8bd18e61b73837811
Wyatt Reed <wyatt@wyattreed.com>
2016-12-09 01:19:14 -0800

    README change

--- secrets/admin_secrets.py
+++
@@ -1,8 +0,0 @@
-# These secrets apply to the whole MongoDB instance and must
-# therefore be kept separate from the per-db client_secrets.py .
-#
-admin_user="wyattr"
-admin_pw="MemeMachine"
-port="27107"
-host="localhost"
-google_key_file = "secrets/google_key_file.json"
